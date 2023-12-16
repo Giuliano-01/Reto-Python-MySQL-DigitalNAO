c74e1162e3fc91af039dc910f9f68acefa949bcf
Giuliano <giulianodepalma@outlook.com>
2023-12-16 13:50:06 -0300

    mejora clase connection

--- Connection.py
+++ Connection.py
@@ -29,39 +29,47 @@ class ConnectionMySQL:
     def create_database(self, database):
 
         try:
-            connection = mysql.connector.connect(
+            with mysql.connector.connect(
                 host=self.host,
                 user=self.user,
                 password=self.password
-            )
-            print("Connected to MySQL Server version", connection.get_server_info())
+            ) as connection:
 
-            if connection.is_connected():
+                print("Connected to MySQL Server version", connection.get_server_info())
 
-                cursor = connection.cursor()
-                cursor.execute("CREATE DATABASE " + database)
+                if connection.is_connected():
 
-                print("Database created\n New databases list:")
-                cursor.execute("SHOW DATABASES")
-                for x in cursor:
-                    print(x)
-                cursor.close()
-                connection.close()
-                print("MySQL connection is closed")
+                    cursor = connection.cursor()
+                    cursor.execute("CREATE DATABASE %s", (database,))
 
-            return None
+                    print("Database created\n New databases list:")
+                    cursor.execute("SHOW DATABASES")
+                    for x in cursor:
+                        print(x)
+                    cursor.close()
+                    connection.close()
+                    print("MySQL connection is closed")
+
+                return None
 
         except mysql.connector.Error as error:
 
-            print("Error while connecting to MySQL".format(error))
+            print(f"Error while connecting to MySQL: {error}")
 
             return None
+
+        finally:
+            if connection.is_connected():
+                cursor.close()
+                connection.close()
+                print("MySQL connection is closed")
 
     ## devuelve la conexion a mysql
     def connect_database(self, attempts=3, delay=2):
 
         attempt = 0
         create_database = True
+
         while attempt < attempts:
             try:
                 connection = mysql.connector.connect(
@@ -76,12 +84,12 @@ class ConnectionMySQL:
 
             except mysql.connector.Error as error:
 
-                if attempt is attempts:
-                    print("Failed to connect, exiting without a connection")
+                if attempt == attempts:
+                    print(f"Failed to connect, exiting without a connection. Error: {error}")
                     return None
 
                 print("Connection failed: Retrying...", attempt, "/", attempts - 1)
-                if (error.errno == errorcode.ER_BAD_DB_ERROR) & (create_database is True):
+                if error.errno == errorcode.ER_BAD_DB_ERROR and create_database:
                     print("Database doesn't exist")
                     print("Do you want to create new one?: ")
                     create_database = choose_yesno()
