29ef8d307a3229e248fc74ea6bb4a7faf482bbf4
Giuliano <giulianodepalma@outlook.com>
2023-12-30 11:04:32 -0300

    update guardado en base de datos

--- InteractDatabase.py
+++ InteractDatabase.py
@@ -1,9 +1,6 @@
 # pip install mysql-connector-python
-import time
 import pandas as pd
 import mysql.connector
-from mysql.connector import errorcode
-from datetime import datetime
 
 
 def convertir_a_datetime(dataframe):
@@ -33,21 +30,24 @@ def choose_yesno():
 
 # Función para determinar el tipo y tamaño del campo basándonos en el valor del diccionario
 def determine_column_type_and_max(column_name, column):
+    try:
+        if column_name == "fecha":
+            return "DATE", ""
+        elif pd.api.types.is_integer_dtype(column):
+            return "INT", "(10)"
+        elif pd.api.types.is_float_dtype(column):
+            return "FLOAT", ""
+        elif pd.api.types.is_string_dtype(column):
+            return "VARCHAR", "(255)"
+        elif pd.api.types.is_datetime64_any_dtype(column):
+            return "DATE", ""
+        elif pd.api.types.is_bool_dtype(column):
+            return "BOOLEAN", ""
+        else:
+            # Puedes agregar más casos según sea necesario
+            return "VARCHAR", "(255)"
 
-    if column_name == "fecha":
-        return "DATE", ""
-    elif pd.api.types.is_integer_dtype(column):
-        return "INT", "(10)"
-    elif pd.api.types.is_float_dtype(column):
-        return "FLOAT", ""
-    elif pd.api.types.is_string_dtype(column):
-        return "VARCHAR", "(255)"
-    elif pd.api.types.is_datetime64_any_dtype(column):
-        return "DATE", ""
-    elif pd.api.types.is_bool_dtype(column):
-        return "BOOLEAN", ""
-    else:
-        # Puedes agregar más casos según sea necesario
+    except:
         return "VARCHAR", "(255)"
 
 
@@ -61,89 +61,160 @@ class InteractMySQL:
     def create_table_with_primary_key(self, table_name, primary_key_name):
 
         print("Creating table..." + table_name)
-        query = f"CREATE TABLE {table_name} ( {primary_key_name} INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY)"
+        query = f"CREATE TABLE {table_name} ({primary_key_name} INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY)"
         try:
 
-            cursor = self.connection.cursor()
-            cursor.execute(query)
+            # El cursor se cerrará automáticamente cuando salga del bloque with, independientemente de si se produjo una excepción o no.
+            with self.connection.cursor() as cursor:
+                cursor.execute(query)
+
             print("Table created")
             return True
 
         except mysql.connector.Error as error:
-            print(error)
+
+            print("Error creating table:", error)
             return False
 
-    # Crea una columna en la tabla
-    def create_column(self, table_name, column_name, column_type, column_max):
+    # Crea una clave foranea
+    def create_foreign_key(self, table_name, column_name, foreign_table, foreign_column):
+        print(f"Creating foreign key in {table_name}/{column_name} referencing {foreign_table}/{foreign_column}...")
 
-        print("Creating column...")
-        query = f"ALTER TABLE {table_name} ADD {column_name} {column_type}{column_max} NOT NULL"
+        query = f"ALTER TABLE {table_name} ADD COLUMN {column_name} INT, ADD CONSTRAINT fk_{table_name} " \
+                f"FOREIGN KEY ({column_name}) REFERENCES {foreign_table}({foreign_column}) ON DELETE CASCADE;"
+
         try:
+            with self.connection.cursor() as cursor:
+                cursor.execute(query)
 
-            cursor = self.connection.cursor()
-            cursor.execute(query)
-            print("Alter created")
+            print("Foreign key created")
             return True
 
         except mysql.connector.Error as error:
-            print(error)
+            print("Error creating foreign key:", error)
             return False
 
-    # Genera una tabla con sus columnas
-    def create_table_from_data(self, table_name, data):
+    # Crea una columna en la tabla
+    def create_column(self, table_name, column_name, data_frame):
 
-        new_data = convertir_a_datetime(data)
+        # Obtener la columna del DataFrame
+        column_data = data_frame[column_name]
 
-        # Asumimos que la primera columna es la clave primaria
-        primary_key_name = new_data.columns[0]
-        self.create_table_with_primary_key(table_name, primary_key_name)
+        # Determinar automáticamente el tipo y máximo de la columna
+        column_type, column_max = determine_column_type_and_max(column_name, column_data)
 
-        for column_name, column_data in new_data.items():
+        print(f"Creating column in {table_name}/{column_name} type {column_type} max {column_max}...")
+        if column_max:
+            query = f"ALTER TABLE {table_name} ADD {column_name} {column_type}{column_max} NOT NULL"
+        else:
+            query = f"ALTER TABLE {table_name} ADD {column_name} {column_type} NOT NULL"
 
-            #Determinamos el tipo y tamaño del campo basándonos en los tipos de datos reales
-            column_type, column_max = determine_column_type_and_max(column_name, column_data)
+        try:
+            with self.connection.cursor() as cursor:
+                cursor.execute(query)
 
-            print("Column name: ", column_name, "Column type: ",column_type)
-            # Creamos la columna en la tabla
-            self.create_column(table_name, column_name, column_type, column_max)
+            print("Column created")
+            return True
 
-    # Inserta los datos a una tabla desde un archivo
-    def insert_data_from_json(self, table_name, data):
+        except mysql.connector.Error as error:
+            print("Error creating column:", error)
+            return False
 
-        print("Insert data in table from json...")
+    # Completa la tabla con datos del data_frame
+    def insert_data_table_from_json(self, table_name, data_frame):
+
+        data = convertir_a_datetime(data_frame)
+
+        cursor = self.connection.cursor()
 
         try:
 
-            # Iterar sobre cada fila del DataFrame
-            for indice, fila in data.iterrows():
-                texto = fila['texto']
-                usuario = fila['usuario']
-                hashtag = ', '.join(fila['hashtags']) if isinstance(fila['hashtags'], list) else fila['hashtags']
-                fecha = fila['fecha']
-                retweets = fila['retweets']
-                favoritos = fila['favoritos']
+            # Obtener las columnas de la tabla
+            cursor.execute(f"DESCRIBE {table_name}")
+            column_descriptions = cursor.fetchall()
 
-                # Crear la consulta de inserción
-                query_insert = f"INSERT INTO {table_name} (texto, usuario, hashtags, fecha, retweets, favoritos) VALUES (%s, %s, %s, %s, %s, %s)"
-                valores = (texto, usuario, hashtag, fecha, retweets, favoritos)
+            # Extraer los nombres de las columnas
+            table_columns = []
+            for column_info in column_descriptions:
+                column_name = column_info[0]
+                table_columns.append(column_name)
 
-                # Ejecutar la consulta de inserción
-                cursor = self.connection.cursor()
-                cursor.execute(query_insert, valores)
+            # Construir la lista de columnas para la consulta
+            columns_list = ', '.join(table_columns)
+            # Construir la lista de marcadores de posición %s para la consulta
+            placeholders = ', '.join(['%s' for _ in table_columns])
 
-            # Confirmar los cambios en la base de datos
+            # Iterar sobre los datos y ejecutar la inserción en la base de datos
+            for index, row in data.iterrows():
+                # Utilizar parámetros de sustitución (%s) en la consulta para evitar SQL injection
+                query = f"INSERT INTO {table_name} ({columns_list}) VALUES ({placeholders})"
+
+                # Obtener los valores de las columnas para el índice actual
+                values = [row[column] for column in table_columns]
+
+                # Ejecutar la consulta con los valores correspondientes
+                cursor.execute(query, tuple(values))
+
+            # Confirmar los cambios en la base de datos fuera del bucle
             self.connection.commit()
+            print("Data inserted successfully")
 
-        except mysql.connector.Error as error:
+        except Exception as e:
+            # En caso de error, realizar un rollback para deshacer los cambios
+            self.connection.rollback()
+            print(f"Error inserting data: {e}")
 
-            if error.errno == errorcode.ER_TABLE_EXISTS_ERROR:
-
-                print("Table already exists. Skipping...")
-
-            else:
-
-                print("Error: ", error)
-                return False
         finally:
+            # Cerrar el cursor al finalizar
+            cursor.close()
 
-            print("Success")
+    def insert_data_table_from_json_with_foreign_key(self, table_name, foreign_key_column, key_column, data_frame):
+
+        data = convertir_a_datetime(data_frame)
+
+        if key_column in data.columns:
+            data[foreign_key_column] = data[key_column]
+
+        cursor = self.connection.cursor()
+
+        try:
+
+            # Obtener las columnas de la tabla
+            cursor.execute(f"DESCRIBE {table_name}")
+            column_descriptions = cursor.fetchall()
+
+            # Extraer los nombres de las columnas
+            table_columns = []
+            for column_info in column_descriptions:
+                column_name = column_info[0]
+                table_columns.append(column_name)
+
+            # Construir la lista de columnas para la consulta
+            columns_list = ', '.join(table_columns)
+            # Construir la lista de marcadores de posición %s para la consulta
+            placeholders = ', '.join(['%s' for _ in table_columns])
+
+            # Iterar sobre los datos y ejecutar la inserción en la base de datos
+            for index, row in data.iterrows():
+                # Utilizar parámetros de sustitución (%s) en la consulta para evitar SQL injection
+                query = f"INSERT INTO {table_name} ({columns_list}) VALUES ({placeholders})"
+
+                # Obtener los valores de las columnas para el índice actual
+                values = [row[column] for column in table_columns]
+                print(values) #[Timestamp('1970-01-01 00:00:00.000000001'), 'La tecnología de la realidad virtual está revolucionando la forma en que experimentamos los videojuegos. #tecnología #videojuegos', '2023-03-13 16:45:00', 23, 87, Timestamp('1970-01-01 00:00:00.000000001')]
+
+                # Ejecutar la consulta con los valores correspondientes
+                cursor.execute(query, tuple(values))
+
+            # Confirmar los cambios en la base de datos fuera del bucle
+            self.connection.commit()
+            print("Data inserted successfully")
+
+        except Exception as e:
+            # En caso de error, realizar un rollback para deshacer los cambios
+            self.connection.rollback()
+            print(f"Error inserting data: {e}")
+
+        finally:
+            # Cerrar el cursor al finalizar
+            cursor.close()
--- Tweets.py
+++ Tweets.py
@@ -13,8 +13,12 @@ interactions_db = InteractMySQL(tweet_connection)
 
 data = pd.read_json("./tweets_2.json")
 
-# interactions_db.create_table_from_data("test", data)
+# La tabla Users tiene una clave primaria id y su username.
 
+interactions_db.insert_data_table_from_json_with_foreign_key("tweets", "user_id", "id", data)
+
+
+# interactions_db.create_table_from_data("test", data)
 # interactions_db.insert_data_from_json("test", data)
 
 
